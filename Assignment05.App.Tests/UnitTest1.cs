8f33f925a4b16328f15b75e04fc861b551e70d0e
tjoms tjomson <thhk@itu.dk>
2020-10-02 14:01:19 +0200

    Add squares method

--- Assignment05.App.Tests/UnitTest1.cs
+++ Assignment05.App.Tests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assignment05.App;
 using Xunit;
 
 namespace Assignment05.App.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(2, 5, new long[]{4,9,16,25})]
+        public void Test_Squares(long lowerBound, long upperBound, long[] expected)
         {
+            var asList = expected.ToList();
+            var actual = ParallelOperations.Squares(lowerBound, upperBound);
 
+            foreach (var square in actual)
+            {
+                Assert.Contains(square, asList);
+            }
+            Assert.Equal(4, actual.Count);
+            
         }
     }
 }
--- Assignment05.App/ParallelOperations.cs
+++ Assignment05.App/ParallelOperations.cs
@@ -1,18 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Linq;
 
-namespace Assignment05
+namespace Assignment05.App
 {
     public class ParallelOperations
     {
         public static ICollection<long> Squares(long lowerBound, long upperBound)
         {
-            throw new NotImplementedException();
-        }
+            var squares = new ConcurrentBag<long>();
+            
+            Parallel.For(lowerBound, upperBound + 1, i =>
+            {
+               squares.Add(i * i); 
+            });
 
+            var returnable = new List<long>(squares);
+
+            return returnable;
+        }
+        
         public static IEnumerable<long> SquaresLinq(int start, int count) =>
-            throw new NotImplementedException();
+  
+        throw new NotImplementedException();
 
         public static void CreateThumbnails(IPictureModule resizer, IEnumerable<string> imageFiles, string outputFolder, Size size)
         {
